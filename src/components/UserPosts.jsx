82ac471db04bb4d8e5b08b9d02d1a421bb92f35b
saikrishna2313 <ndsdkvarma6823@gmail.com>
2024-06-03 18:41:53 -0700

    Your descriptive commit message

--- src/components/UserPosts.jsx
+++ src/components/UserPosts.jsx
@@ -18,7 +18,7 @@ const UserPosts = async({user}) => {
         <h1 className="text-xl font-semibold text-blue-500">Blogs</h1>
 
     </div> */}
-   <section key="jey" className="grid border-t-2 max-sm:mx-1 grid-cols-3 justify-center items-center  overflow-x-hidden mx-auto">
+   <section  className="grid border-t-2 max-sm:mx-1 grid-cols-3 justify-center items-center  overflow-x-hidden mx-auto">
    
       {
         blogs.map((blog)=>{
@@ -29,7 +29,7 @@ const UserPosts = async({user}) => {
                    <div>
                    <Image width={200} quality={100} height={100} src={blog?.image} className="object-cover w-[200px] h-[100px]"/>
                    </div>
-                   <div className="absolute  flex justify-center bg-black bg-opacity-[30%] items-center top-0 left-0  right-0 w-full h-full">
+                   <div key={blog?.authorId} className="absolute  flex justify-center bg-black bg-opacity-[30%] items-center top-0 left-0  right-0 w-full h-full">
                    <h1 className="p-2 text-sm font-semibold  rounded-xl    text-slate-200 ">{blog?.category}</h1>
                    </div>
                 </Link>
