345924238d980b0b8f6183382f73a91bdd8e862c
Sai Krishna <142757076+saikrishna2313@users.noreply.github.com>
2024-06-04 00:42:52 -0700

    Update AddComent.jsx

--- src/app/Forms/AddComent.jsx
+++ src/app/Forms/AddComent.jsx
@@ -32,7 +32,7 @@ const AddComent = ({blogId}) => {
     <section className="">
         <form action={add} ref={ref} className="max-w-4xl px-3 gap-3 flex justify-start items-center rounded shadow-lg  mx-auto">
     <div className="bg -slate-200 p-2 rounded-full">
-    <Image width={30} height={30} src={session?.data?.user?.image} className="w-8 border-2 border-slate-900 rounded-full h-8"/>
+    <Image width={30} height={30} src={session?.data?.user?.image} className='h-8 w-8 rounded-full border-2'/>
     </div>
     <textarea required  rows={1} name="text" type="text" className="w-full my-1 px-3 mx-1 py-2 shadow-md outline-none" placeholder="Add Comment"/>
     <button type="submit"><FaCircleArrowRight className="w-6 hover:text-blue-400 rotate-90 h-6"/></button>
